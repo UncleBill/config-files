4ffe4d4df26e7ffbe4a0693ed09af393b3bfe391
Shiyong Chen <billbill290@gmail.com>
2013-05-26 17:32:55 +0800

    add installer and a tiny fix

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# vim config files
+
+for file in *.vim
+do
+    ln -f ${file} ~/.vim/${file}
+done
+
+# awesomewm config
+ln -f ./dotfiles/rc.lua ~/.config/awesome/rc.lua
+ln -f ./dotfiles/.zshrc ~/.zshrc
+ln -f ./dotfiles/.conkyrc ~/.conkyrc
