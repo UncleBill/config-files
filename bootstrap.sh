55add51cec2afbff0a0f70b16572af0203079753
unclebill <unclebill@mindhand.io>
2020-06-11 14:18:07 +0800

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+brew install ripgrep
+brew install tig
+brew install the_silver_searcher
+brew install ranger
+brew install fzf
+brew install quickjs
+brew install go
+
+git clone https://github.com/TomaszRewak/C-sharp-stack-only-json-parser.git
+git clone https://github.com/acornjs/acorn.git
+git clone https://github.com/react-native-community/async-storage.git
+git clone https://github.com/Homebrew/brew.git
+git clone https://github.com/sdegutis/committer.git
+git clone https://github.com/facebook/create-react-app.git
+git clone https://github.com/firefox-devtools/devtools-core.git
+git clone https://github.com/evanw/esbuild.git
+git clone https://github.com/facebook/flow.git
+git clone https://github.com/flutter/flutter.git
+git clone https://github.com/facebook/jest.git
+git clone https://github.com/nodejs/node-core-utils.git
+git clone https://github.com/rime/plum.git
+git clone https://github.com/facebook/react.git
+git clone https://github.com/facebook/react-native.git
+git clone https://github.com/react-spring/react-spring.git
+git clone https://github.com/reactjs/reactjs.org.git
+git clone https://github.com/facebook/reason.git
+git clone https://github.com/vim/vim.git
+git clone https://github.com/macvim-dev/macvim.git
+git clone https://github.com/vuejs/vue.git
+git clone git@github.com:UncleBill/whitespire.git
+git clone https://github.com/v8/v8.git
+git clone https://github.com/gatsbyjs/gatsby.git
+git clone https://github.com/swc-project/swc.git
+git clone https://github.com/babel/babel.git
+git clone https://github.com/immutable-js/immutable-js.git
+git clone https://github.com/sveltejs/svelte.git
